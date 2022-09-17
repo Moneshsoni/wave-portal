8984a145ea3b61cbeabe01b25cb4b60e74065e85
Moneshsoni <sonimonesh115@gmail.com>
2022-09-17 18:13:48 +0530

    add update file deploy and add artifacts file

--- my-wave-portal/scripts/run.js
+++ my-wave-portal/scripts/run.js
@@ -1,26 +1,28 @@
 const main = async () => {
-    const [owner, randomPerson] = await hre.ethers.getSigners();
+    // const [owner, randomPerson] = await hre.ethers.getSigners();
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
     const waveContract = await waveContractFactory.deploy();
     await waveContract.deployed();
    
     console.log("Contract deployed to:", waveContract.address);
-    console.log("Contract deployed by  owner by address:", owner.address);
-    console.log("Random address",randomPerson.address);
+    // console.log("Contract deployed by  owner by address:", owner.address);
+    // console.log("Random address",randomPerson.address);
    
     let waveCount;
     waveCount = await waveContract.getTotalWaves();
-   
-    let waveTxn = await waveContract.wave();
+    console.log("Our total waves are!",waveCount);
+
+    let waveTxn = await waveContract.wave("Hi this is first wave messages!");
     await waveTxn.wait();
-   
+
     waveCount = await waveContract.getTotalWaves();
+    console.log("Our total wave after the wave: ",waveCount);
 
+    let allWaves = await waveContract.getTotalWaves();
+    console.log("Get all waves are !",allWaves);
 
-    console.log("Our total waves after the wave:",waveCount);
-    waveTxn = await waveContract.connect(randomPerson).wave();
-    console.log("Random Addreess",waveTxn.address);
-    
+  
+       
   };
    
   const runMain = async () => {
